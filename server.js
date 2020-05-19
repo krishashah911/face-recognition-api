9fa17c27e753bb3d42e1abb07d28bfcd9b516af8
krishashah911 <49025746+krishashah911@users.noreply.github.com>
2020-05-19 14:10:32 +0530

    update changes

--- server.js
+++ server.js
@@ -109,8 +109,8 @@ app.put('/image', (req,res) => {
   	.catch(err => res.status(400).json('Unable to get entries'))
 })
 
-app.listen(3000, () => {
-	console.log(`app is running on port 3000`);
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
